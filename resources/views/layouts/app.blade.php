6e6b507d6b77d844bf86af99bf5e9d5d77912070
thandarannie <thandarannie12@gmail.com>
2022-10-05 09:28:08 +0630

    fix error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
 
-    {{-- fontawasome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
     <!-- MDB -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css" rel="stylesheet" />
 
@@ -34,8 +32,6 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 
-    {{-- workout css --}}
-    <link rel="stylesheet" href="{{asset('css/workout.css')}}">
     <!-- Select2 -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
@@ -56,16 +52,8 @@
         <header class="header" id="header">
             <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
             <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
-
-
             <div class="dropdown">
-                <button
-                class="btn btn-secondary dropdown-toggle"
-                type="button"
-                id="dropdownMenuButton"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-                >
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false" >
                 <i class="fa-solid fa-user"></i>&nbsp;&nbsp; {{Auth()->user()->name}}
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -82,7 +70,7 @@
                     </a>
                 </li>
                 </ul>
-              </div>
+            </div>
         </header>
         <div class="l-navbar" id="nav-bar">
             <nav class="nav">
@@ -131,13 +119,10 @@
 
                     </div>
                 </div>
-                {{-- <form action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    @method('POST')
-                    <button type="submit" class="btn btn-secondary" onclick="return confirm('Are you sure you want to logout this ?')">
-                        <i class='bx bx-log-out nav_icon'></i>
-                    </button>
-                </form> --}}
+                <a href="#" class="nav_link">
+                    <i class='bx bx-log-out nav_icon'></i>
+                    <span class="nav_name">SignOut</span>
+                </a>
             </nav>
         </div>
         <!--Container Main start-->
@@ -161,7 +146,6 @@
     <script src="https://cdn.datatables.net/fixedheader/3.2.3/js/dataTables.fixedHeader.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <!-- Sweet Alert -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -180,6 +164,7 @@
     <script>
 
        $(document).ready(function() {
+
         $(document).on('click','.sign_out', function (e) {
                 e.preventDefault();
 
@@ -203,7 +188,7 @@
                 })
 
         $(document).on('submit', 'form', function() {
-
+           
             $('button').attr('disabled', 'disabled');
         });
 
