2fd33100d698e638ecd4455b3ad309326e3d70ca
Nyein Chan <konyeinchanko.it@gmail.com>
2022-10-12 10:18:10 +0630

    fix234

--- resources/views/admin/trainers/create.blade.php
+++ resources/views/admin/trainers/create.blade.php
@@ -16,18 +16,23 @@
                 </div>
                 <div class="mt-4">
                     <label class="" for="training_type">Training Type</label> <br>
-
                     <select class="form-control " name="training_type" id="training_type">
                         <option value="weight_gain">Weight Gain</option>
                         <option value="weight_loss">Weight Loss</option>
                         <option value="body_beauty">Body Beauty</option>
                     </select>
-
-
                 </div>
                 <div class="mt-4">
                     <label for="address">Address</label>
                     <textarea name="address" id="" cols="30" rows="5" class="form-control"></textarea>
+                </div>
+                <div class="mt-4">
+                    <label for="password">Password</label>
+                    <input type="number" class="form-control" name="password" id="password">
+                </div>
+                <div class="mt-4">
+                    <label for="password_confirmation">Confirmed Password</label>
+                    <input type="number" class="form-control" name="password_confirmation" id="password_confirmation">
                 </div>
 
                 <div class="float-end mt-4">
